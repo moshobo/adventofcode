befcf5aba4b8c5039eb87ef4047ae1421d4df440
moshobo <jmiahchikota@gmial.com>
2020-12-14 22:01:54 -0500

    Day 14 - Finished only pt. I at night, but managed to get both parts without any help. Learnt a lot about binary!

--- Day 13/day_main.py
+++ Day 13/day_main.py
@@ -1,2 +1,17 @@
 data = open("input.txt", "r")
-instructions = [line.strip() for line in data]
+notes = [line.strip() for line in data]
+
+start_time = int(notes[0])
+buses = [bus for bus in notes[1].split(',') if bus != 'x']
+times = [start_time % int(i) for i in buses]
+
+time = start_time
+good_buses = []
+while not good_buses:
+    good_buses = [bus for bus in buses if time % int(bus) == 0]
+    if good_buses:
+        print(good_buses)
+    else:
+        time += 1
+
+print("Part I -- ", int(good_buses[0]) * abs(start_time - time))
